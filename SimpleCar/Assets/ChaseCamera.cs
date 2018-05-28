2424d498d04fa61f9a97c6d0dca1aae1aa300a7a
Unknown <konata016@gmail.com>
2018-05-28 18:07:17 +0900

    新しいプロジェクト

---
+++ SimpleCar/Assets/ChaseCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseCamera : MonoBehaviour {
+
+    public GameObject PL;
+    public int My_y = 3;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 camera = this.transform.position;
+
+        camera.x = PL.transform.position.x;
+        camera.y = PL.transform.position.y + My_y;
+        camera.z = PL.transform.position.z - 10;
+
+        transform.position = camera;
+    }
+}
---
+++ SimpleCar/Assets/Warp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour
+{
+
+    public GameObject PL;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        PL.gameObject.transform.position = new Vector3(-7f, 0f, 0f);
+    }
+}
